83c9edeeb27644ee41c6746e408137f8f6a86936
Michael Bohan <mbohan@codeaurora.org>
2010-06-02 12:19:40 -0700

    ARM: msm: board-msm7x30: Add display support for SURF/FFA
    The board uses a Toshiba Wega MDDI client.
    
    Change-Id: Id30127f309432ab4204d02a13e6c36406bc063fd
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/board-msm7x30.c
+++ arch/arm/mach-msm/board-msm7x30.c
@@ -336,6 +336,14 @@ static void __init msm7x30_fixup(struct machine_desc *desc, struct tag *tags,
 	mi->bank[0].size = (51*1024*1024);
 }
 
+
+extern void __init msm7x30_allocate_fbmem(void);
+
+static void __init msm7x30_reserve(void)
+{
+	msm7x30_allocate_fbmem();
+}
+
 MACHINE_START(MSM7X30_SURF, "QCT MSM7X30 SURF")
 #ifdef CONFIG_MSM_DEBUG_UART
 #endif
@@ -345,6 +353,7 @@ MACHINE_START(MSM7X30_SURF, "QCT MSM7X30 SURF")
 	.init_irq = msm7x30_init_irq,
 	.init_machine = msm7x30_init,
 	.timer = &msm_timer,
+	.reserve = msm7x30_reserve,
 MACHINE_END
 
 MACHINE_START(MSM7X30_FFA, "QCT MSM7X30 FFA")
@@ -355,6 +364,7 @@ MACHINE_START(MSM7X30_FFA, "QCT MSM7X30 FFA")
 	.init_irq = msm7x30_init_irq,
 	.init_machine = msm7x30_init,
 	.timer = &msm_timer,
+	.reserve = msm7x30_reserve,
 MACHINE_END
 
 MACHINE_START(MSM7X30_FLUID, "QCT MSM7X30 FLUID")
@@ -365,4 +375,5 @@ MACHINE_START(MSM7X30_FLUID, "QCT MSM7X30 FLUID")
 	.init_irq = msm7x30_init_irq,
 	.init_machine = msm7x30_init,
 	.timer = &msm_timer,
+	.reserve = msm7x30_reserve,
 MACHINE_END
