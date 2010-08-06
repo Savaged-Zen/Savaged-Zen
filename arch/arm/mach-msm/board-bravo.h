2798a17ec813d0fcf74ecb39f232a304afd8564c
Kali- <kalimero@ngi.it>
2010-08-07 00:11:52 +0200

    [bravo] add support for Sony TFT panel

--- arch/arm/mach-msm/board-bravo.h
+++ arch/arm/mach-msm/board-bravo.h
@@ -92,7 +92,7 @@
 /* Compass */
 #define BRAVO_GPIO_COMPASS_INT_N	153
 #define BRAVO_GPIO_COMPASS_RST_N	107
-#define BRAVO_PROJECT_NAME          "bravo"
+#define BRAVO_PROJECT_NAME"bravo"
 #define BRAVO_LAYOUTS { 			   \
 	{ {-1,  0, 0}, { 0, -1,  0}, {0, 0,  1} }, \
 	{ { 0, -1, 0}, { 1,  0,  0}, {0, 0, -1} }, \
@@ -117,15 +117,16 @@
 #define BRAVO_GPIO_FLASHLIGHT_FLASH	84
 
 /* keypad */
-#define BRAVO_GPIO_KP_MKOUT0            33
-#define BRAVO_GPIO_KP_MKOUT1            32
-#define BRAVO_GPIO_KP_MKOUT2            31
-#define BRAVO_GPIO_KP_MPIN0             42
-#define BRAVO_GPIO_KP_MPIN1             41
-#define BRAVO_GPIO_KP_MPIN2             40
+#define BRAVO_GPIO_KP_MKOUT0		33
+#define BRAVO_GPIO_KP_MKOUT1		32
+#define BRAVO_GPIO_KP_MKOUT2		31
+#define BRAVO_GPIO_KP_MPIN0		42
+#define BRAVO_GPIO_KP_MPIN1		41
+#define BRAVO_GPIO_KP_MPIN2		40
 
 #define BRAVO_GPIO_LED_3V3_EN		85
 #define BRAVO_GPIO_LCD_RST_N		29
+#define MAHIMAHI_GPIO_LCD_ID0		147
 
 /* 3.5mm remote control key interrupt shutdown signal */
 #define BRAVO_GPIO_35MM_KEY_INT_SHUTDOWN	19
@@ -135,5 +136,31 @@
 #define BRAVO_GPIO_BATTERY_DETECTION		39
 #define BRAVO_GPIO_BATTERY_CHARGER_EN		22
 #define BRAVO_GPIO_BATTERY_CHARGER_CURRENT	16
+
+/* display relative */
+#define MAHIMAHI_LCD_SPI_CLK            (17)
+#define MAHIMAHI_LCD_SPI_DO             (18)
+#define MAHIMAHI_LCD_SPI_CSz            (20)
+#define MAHIMAHI_LCD_RSTz               (29)
+#define MAHIMAHI_LCD_R1                 (114)
+#define MAHIMAHI_LCD_R2                 (115)
+#define MAHIMAHI_LCD_R3                 (116)
+#define MAHIMAHI_LCD_R4                 (117)
+#define MAHIMAHI_LCD_R5                 (118)
+#define MAHIMAHI_LCD_G0                 (121)
+#define MAHIMAHI_LCD_G1                 (122)
+#define MAHIMAHI_LCD_G2                 (123)
+#define MAHIMAHI_LCD_G3                 (124)
+#define MAHIMAHI_LCD_G4                 (125)
+#define MAHIMAHI_LCD_G5                 (126)
+#define MAHIMAHI_LCD_B1                 (130)
+#define MAHIMAHI_LCD_B2                 (131)
+#define MAHIMAHI_LCD_B3                 (132)
+#define MAHIMAHI_LCD_B4                 (133)
+#define MAHIMAHI_LCD_B5                 (134)
+#define MAHIMAHI_LCD_PCLK               (135)
+#define MAHIMAHI_LCD_VSYNC              (136)
+#define MAHIMAHI_LCD_HSYNC              (137)
+#define MAHIMAHI_LCD_DE                 (138)
 
 #endif /* __ARCH_ARM_MACH_MSM_BOARD_BRAVO_H */
