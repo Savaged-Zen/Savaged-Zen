1e69f0392c8ab207d192b23dc901335918e2de0e
Brandon Berhent <bbedward@gmail.com>
2011-01-26 13:00:58 -0500

    Revert "Merge branch 'classic-rcu'"
    This reverts commit ba9bd060c45632bdd8f14db26073b5fc7b6a91ba, reversing
    changes made to eed3df06b76dbc6f89a78bf2280031b119a37031.

--- include/linux/hardirq.h
+++ include/linux/hardirq.h
@@ -169,23 +169,6 @@ static inline void rcu_nmi_exit(void)
 {
 }
 
-#elif defined(CONFIG_CLASSIC_RCU)
-static inline void rcu_irq_enter(void)
-{
-}
-
-static inline void rcu_irq_exit(void)
-{
-}
-
-static inline void rcu_nmi_enter(void)
-{
-}
-
-static inline void rcu_nmi_exit(void)
-{
-}
-
 #else
 extern void rcu_irq_enter(void);
 extern void rcu_irq_exit(void);
