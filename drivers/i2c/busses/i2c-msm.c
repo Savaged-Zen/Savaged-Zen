568e592e226fc27a71b4625e2227a36834f2f881
Steve Kondik <shade@chemlab.org>
2011-03-09 02:26:29 -0500

    msm: i2c: Hold mutex around i2c_xfer and during suspend/resume.

--- drivers/i2c/busses/i2c-msm.c
+++ drivers/i2c/busses/i2c-msm.c
@@ -24,6 +24,7 @@
 #include <linux/delay.h>
 #include <linux/io.h>
 #include <linux/wakelock.h>
+#include <linux/mutex.h>
 #include <mach/system.h>
 
 #define DEBUG 0
@@ -77,6 +78,7 @@ struct msm_i2c_dev {
 	void                *complete;
 	struct wake_lock    wakelock;
 	bool                is_suspended;
+	struct mutex	    mlock;
 };
 
 #if DEBUG
@@ -335,6 +337,7 @@ msm_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 	if (WARN_ON(!num))
 		return -EINVAL;
 
+	mutex_lock(&dev->mlock);
 	/*
 	 * If there is an i2c_xfer after driver has been suspended,
 	 * grab wakelock to abort suspend.
@@ -406,7 +409,7 @@ err:
 	clk_disable(dev->clk);
 	if (dev->is_suspended)
 		wake_unlock(&dev->wakelock);
-
+	mutex_unlock(&dev->mlock);
 	return ret;
 }
 
@@ -476,6 +479,7 @@ msm_i2c_probe(struct platform_device *pdev)
 		goto err_ioremap_failed;
 	}
 
+	mutex_init(&dev->mlock);
 	spin_lock_init(&dev->lock);
 	wake_lock_init(&dev->wakelock, WAKE_LOCK_SUSPEND, "i2c");
 	platform_set_drvdata(pdev, dev);
@@ -540,6 +544,10 @@ msm_i2c_remove(struct platform_device *pdev)
 	struct msm_i2c_dev	*dev = platform_get_drvdata(pdev);
 	struct resource		*mem;
 
+	mutex_lock(&dev->mlock);
+	dev->is_suspended = true;
+	mutex_unlock(&dev->mlock);
+	mutex_destroy(&dev->mlock);
 	platform_set_drvdata(pdev, NULL);
 	enable_irq(dev->irq);
 	free_irq(dev->irq, dev);
@@ -558,10 +566,12 @@ static int msm_i2c_suspend_noirq(struct device *device)
 	struct platform_device *pdev = to_platform_device(device);
 	struct msm_i2c_dev *dev = platform_get_drvdata(pdev);
 
-	/* Block to allow any i2c_xfers to finish */
-	i2c_lock_adapter(&dev->adapter);
-	dev->is_suspended = true;
-	i2c_unlock_adapter(&dev->adapter);
+	if (dev) {
+		/* Block to allow any i2c_xfers to finish */
+		mutex_lock(&dev->mlock);
+		dev->is_suspended = true;
+		mutex_unlock(&dev->mlock);
+	}
 	return 0;
 }
 
@@ -569,10 +579,12 @@ static int msm_i2c_resume_noirq(struct device *device) {
 	struct platform_device *pdev = to_platform_device(device);
 	struct msm_i2c_dev *dev = platform_get_drvdata(pdev);
 
-	/* Block to allow any i2c_xfers to finish */
-	i2c_lock_adapter(&dev->adapter);
-	dev->is_suspended = false;
-	i2c_unlock_adapter(&dev->adapter);
+	if (dev) {
+		/* Block to allow any i2c_xfers to finish */
+		mutex_lock(&dev->mlock);
+		dev->is_suspended = false;
+		mutex_unlock(&dev->mlock);
+	}
 	return 0;
 }
 
