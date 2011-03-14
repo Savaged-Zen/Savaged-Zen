bc0e4351e487dc80c02f6cc0bdbefc096ab7b292
Brandon Berhent <bbedward@gmail.com>
2011-03-14 19:20:40 -0400

    supersonic: Lower wifi voltage (intersectRaven)

--- arch/arm/mach-msm/board-supersonic-mmc.c
+++ arch/arm/mach-msm/board-supersonic-mmc.c
@@ -198,7 +198,7 @@ static unsigned int supersonic_wifi_status(struct device *dev)
 }
 
 static struct msm_mmc_platform_data supersonic_wifi_data = {
-	.ocr_mask		= MMC_VDD_28_29,
+	.ocr_mask		= MMC_VDD_20_21,
 	.built_in		= 1,
 	.status			= supersonic_wifi_status,
 	.register_status_notify	= supersonic_wifi_status_register,
